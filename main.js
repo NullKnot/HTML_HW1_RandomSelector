baf9d350efab1b2e5109ca08117adaca5dc30520
NullKnow <terroson8422@gmail.com>
2016-03-10 05:04:50 +0800

    commit1

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function showAlert(){
     var index = Math.floor(4*Math.random());
+    var photoType = new Array("法國.jpg", "澳大利亞.jpg", "日本.jpg", "新加坡.jpg");
+    
     document.getElementsByTagName("h1")[0].innerHTML = document.getElementsByTagName("li")[index].innerHTML;
+    document.body.background=photoType[index];
 }
